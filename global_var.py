f209f614ae630132a420150db00659a44e18ecf3
Jose Juan Cruz <juanolmos05_@comuinidad.unam.mx>
2023-04-17 19:28:47 -0600

    Renombre id_program a variables globales

---
+++ global_var.py
@@ -0,0 +1,18 @@
+""" Keys para leer los datos de los archivos .csv en un DataFrame 
+    y se ejecute correctamente el programa principal. 
+    Pueden ser diferentes según el nombre de las columnas de la base de datos a analizar
+
+    Variables globrales para funcionamiento del programa principal
+"""
+code = 'Code'
+product = 'Producto'
+price_csv = 'Precio unit'
+price = 'Precio'
+name = 'Cotización'
+inv = 'Inventario'
+best_price = 'Mejor precio'
+clear = 'clear'     # Para MacOs y Linux 'clear' para Windows 'cls'
+Error1 = 'Ocurrio un error'
+# path_replace = 'C:/Users/ameri/Documents/Inventario/Cotizaciones SIMA/' # Para windows
+path_replace = '/home/jjuanol05/Documentos/SIMA/Codigo/Cotizaciones SIMA/'
+path_export = '/home/jjuanol05/Documentos/SIMA/Codigo/Resul/compare_list.csv'
