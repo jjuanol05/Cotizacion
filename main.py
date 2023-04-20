7351c31b2f25dffa30337fa7bea0f88dea12dee2
Jose Juan Cruz <juanolmos05_@comuinidad.unam.mx>
2023-04-20 15:41:59 -0600

    cambié el programa principal para comparar 5 cotizaciones

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Install tkinter   with sudo apt-get install python3.10-tk (for linux)
 # Install tkinter   with brew install python-tk@3.10 (for MAC)
 # Install pandas    with pip install pandas
-# Install numpy     with pip install numpy
 # Install pathlib   with pip install pathlib
 
 from tkinter.filedialog import askopenfilename
@@ -15,7 +14,28 @@ import os
                 resul: el mejor
 """
 
-os.system(gv.clear)
+os.system(gv.CLEAR)
+
+def Compare3(file1, file2, file3):
+    
+    cot1 = ct.Cotizacion(file1)
+    cot2 = ct.Cotizacion(file2)
+    cot3 = ct.Cotizacion(file3)
+
+    file = cot1.Compare(cot2)
+    
+    if  file == file1:
+        print(f'\nCOMPARACIÓN 1:\n{cot1}')
+        file = cot1.Compare(cot3)
+        print(f'\nCOMPARACIÓN 2:\n{cot1}')
+
+    elif file == file2:
+        cot2.Compare(cot1)
+        print(f'\nCOMPARACIÓN 1:\n{cot2}')
+        file = cot2.Compare(cot3)
+        print(f'\nCOMPARACIÓN 2:\n{cot2}')
+    
+    return file
 
 try:
     # file1 = askopenfilename()
@@ -25,27 +45,16 @@ try:
     file1 = '/home/jjuanol05/Documentos/SIMA/Codigo/Cotizaciones SIMA/Cotizacion Tamex 2087708.csv'
     file2 = '/home/jjuanol05/Documentos/SIMA/Codigo/Cotizaciones SIMA/Ventas.csv'
     file3 = '/home/jjuanol05/Documentos/SIMA/Codigo/Cotizaciones SIMA/641122.csv'
+    file4 = '/home/jjuanol05/Documentos/SIMA/Codigo/Cotizaciones SIMA/Cotizacion Tamex 2087708.csv'
+    file5 = '/home/jjuanol05/Documentos/SIMA/Codigo/Cotizaciones SIMA/641122.csv'
 
-    cot1 = ct.Cotizacion(file1)
-    cot2 = ct.Cotizacion(file2)
-    cot3 = ct.Cotizacion(file3)
+    file = Compare3(file1, file2, file3)
 
-    file = cot1.Comparar(cot2)
-    
-    if  file == file1:
-        print(f'\nCOMPARACIÓN 1:\n{cot1}')
-        cot1.Comparar(cot3)
-        print(f'\nCOMPARACIÓN 2:\n{cot1}')
-
-    elif file == file2:
-        cot2.Comparar(cot1)
-        print(f'\nCOMPARACIÓN 1:\n{cot2}')
-        cot2.Comparar(cot3)
-        print(f'\nCOMPARACIÓN 2:\n{cot2}')
+    file_final = Compare3(file, file4, file5)
 
 except IndexError:
     print(f'\nNO SE PUDEN COMPARAR COTIZACIONES, NúMERO DE PRODUCTOS DIFERENTES\nIntente de nuevo')
     exit()
 except KeyboardInterrupt:
-    os.system(gv.clear)
+    os.system(gv.CLEAR)
     exit()
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import cotizacion as ct
 import global_var as gv
 import os
 
-os.system(gv.clear)
+os.system(gv.CLEAR)
 
 try:
     file1 = askopenfilename()
@@ -14,7 +14,7 @@ try:
     cot2 = ct.Cotizacion(file2)
     # cot3 = COT.Cotizacion(file3)
 
-    cot1.Comparar(cot2)
+    cot1.Compare(cot2)
     print(f'\n{cot1}')
     # cot1.Count()
 
@@ -28,5 +28,5 @@ except IndexError:
     print(f'\nNO SE PUDEN COMPARAR COTIZACIONES, NúMERO DE PRODUCTOS DIFERENTES\nIntente de nuevo')
     exit()
 except KeyboardInterrupt:
-    os.system(gv.clear)
+    os.system(gv.CLEAR)
     exit()
